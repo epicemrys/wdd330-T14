8678e15bbb51c29261496fd5e5d921d62139b250
Sandra Rodriguez <jackiesophia13@gmail.com>
2026-01-15 18:46:51 -0500

    W02 dynamic product detail page

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,9 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs"; // Import getLocalStorage
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  // e.target.dataset.id should contain the ID of the product
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// Listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
