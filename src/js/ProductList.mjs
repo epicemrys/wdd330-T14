d6b3f09323fd65d86b5a9db26b4770301ec7d7d4
AMbonani <refilwe.given@gmail.com>
2026-01-25 00:28:01 +0200

    w2 changes

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,33 +1,37 @@
-import { renderListWithTemplate } from './utils.mjs';
+import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `
-        <div class="product-card">
-            <img src="${product.image}" alt="${product.name}" class="product-image">
-            <h3 class="product-name">${product.name}</h3>
-            <p class="product-description">${product.description}</p>
-            <p class="product-price">$${product.price}</p>
-        </div>
-    `;
+  return `
+    <li class="product-card">
+      <a href="/product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="${product.Name}" />
+        <h3 class="card__brand">${product.Brand}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
 }
 
 export default class ProductList {
-    constructor(category, dataSource, listElement) {
-        this.category = category;
-        this.dataSource = dataSource;
-        this.listElement = listElement;
-        this.products = [];
-    }
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
 
-    async init() {
-        try {
-            this.products = await this.dataSource.getData();
-            this.renderProductList();
-        } catch (error) {
-            console.error('Error initializing ProductList:', error);
-        }
-    }
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
-    }
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(
+      productCardTemplate,
+      this.listElement,
+      list,
+      "afterbegin",
+      true
+    );
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,12 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents'); //instance of ProductData
-const element = document.querySelector('product-list');
-const productList = new ProductList('tents', dataSource, element);
 
+const dataSource = new ProductData("tents");
+
+
+const listElement = document.querySelector(".product-list");
+
+
+const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
-
-//Fetch data and log to console
-async function fetchProductData() {
-    try {
-        const data = await productData.getData();
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching product data:', error);
-    }
-}
-
-fetchProductData();
